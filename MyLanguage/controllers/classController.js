a4421cbc635af727f4a0939fb978cd1fc0ae5d62
alfatt <isfanialfattah@gmail.com>
2020-12-17 23:20:37 +0700

    crud student dan class

--- MyLanguage/controllers/classController.js
+++ MyLanguage/controllers/classController.js
@@ -5,6 +5,44 @@ class ClassController {
         LanguageClass.findAll()
          .then(data => res.render('classPage/classView', {data}))
     }
+    static getAdd(req, res) {
+        res.render('classPage/addClassForm')
+    }
+    static postAdd(req, res) {
+        let newClass = {
+            class_name: req.body.class_name,
+            schedule: req.body.schedule,
+            description: req.body.description,
+            price: req.body.price
+        }
+        LanguageClass.create(newClass)
+         .then(() => res.redirect('/classes'))
+         .catch(err => res.send(err))
+    }
+    static getEdit(req, res) {
+        let id = +req.params.class_id
+        LanguageClass.findByPk(id)
+         .then(data => res.render('classPage/classEditForm', {data}))
+         .catch(err => res.send(err))
+    }
+    static postEdit(req, res) {
+        let id = +req.params.class_id
+        let newClass = {
+            class_name: req.body.class_name,
+            schedule: req.body.schedule,
+            description: req.body.description,
+            price: req.body.price
+        }
+        LanguageClass.update(newClass, {where: {id}})
+         .then(() => res.redirect('/classes'))
+         .catch(err => res.send(err))
+    }
+    static delete(req, res) {
+        let id = +req.params.class_id
+        LanguageClass.destroy({where: {id}})
+        .then(() => res.redirect('/classes'))
+        .catch(err => res.send(err))
+    }
 }
 
 module.exports = ClassController
--- MyLanguage/routes/studentRoute.js
+++ MyLanguage/routes/studentRoute.js
@@ -3,5 +3,10 @@ const router = express.Router()
 const StudentController = require("../controllers/studentController")
 
 router.get("/students", StudentController.list)
+router.get('/students/add', StudentController.getAdd)
+router.post('/students/add', StudentController.postAdd)
+router.get('/students/edit/:student_id', StudentController.getEdit)
+router.post('/students/edit/:student_id', StudentController.postEdit)
+router.get('/students/delete/:student_id', StudentController.delete)
 
 module.exports = router
